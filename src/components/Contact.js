c1b54c06c8f36b6f6546ee68f0bca6ab270695c0
sa17shoaib <shoaibalam.9836@gmail.com>
2023-07-28 18:20:25 +0530

    commit again

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,47 +4,53 @@ const Contact = () => {
   return (
     <>
       <Hero text="Contact Us" />
-      <form className="mx-auto my-5" style={{ width: "500px" }}>
-        <div className="mb-3">
-          <label for="exampleInputEmail1" className="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-            placeholder="name@email.com"
-          />
-        </div>
-        <div className="mb-3">
-          <label for="exampleInputPassword1" className="form-label">
-            Your Name
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="exampleInputPassword1"
-            placeholder="Barry Allen"
-          />
-        </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 offset-lg-2 my-5">
+            <form>
+              <div className="mb-3">
+                <label for="exampleInputEmail1" className="form-label">
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="name@email.com"
+                />
+              </div>
+              <div className="mb-3">
+                <label for="exampleInputPassword1" className="form-label">
+                  Your Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="Barry Allen"
+                />
+              </div>
 
-        <div class="mb-3">
-          <label for="exampleFormControlTextarea1" class="form-label">
-            Describe your issue in detail
-          </label>
-          <textarea
-            class="form-control"
-            id="exampleFormControlTextarea1"
-            rows="3"
-            placeholder="For e.g., Unable to view details"
-          ></textarea>
-        </div>
+              <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">
+                  Describe your issue in detail
+                </label>
+                <textarea
+                  class="form-control"
+                  id="exampleFormControlTextarea1"
+                  rows="3"
+                  placeholder="For e.g., Unable to view details"
+                ></textarea>
+              </div>
 
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
